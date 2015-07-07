62e869dd2f066d0a7684fbaf0b3a55e87f4f64c5
RePierre <petru.rebeja@gmail.com>
2015-07-07 15:14:01 +0300

    Broadline agenda.

---
+++ 1.ThreadingBasics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetAsync.ThreadingBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
